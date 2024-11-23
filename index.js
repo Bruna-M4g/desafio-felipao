c2e7fca0ef65c6f5b7f5b92674ed522ccb27293a
Bruna-M4g <magalhaesbruna.m@gmail.com>
2024-11-23 15:22:45 -0300

    Final

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log ("Oi")
+let xp = 10000; 
+let classificacao;
+
+if (xp < 1000) {
+    classificacao = "Ferro";
+} else if (xp <= 2000) {
+    classificacao = "Bronze";
+} else if (xp <= 5000) {
+    classificacao = "Prata";
+} else if (xp <= 7000) {
+    classificacao = "Ouro";
+} else if (xp <= 8000) {
+    classificacao = "Platina";
+} else if (xp <= 9000) {
+    classificacao = "Ascendente";
+} else if (xp <= 10000) {
+    classificacao = "Imortal";
+}
+
+console.log("O herói de nome Corvo está no nível de " + classificacao);
+
